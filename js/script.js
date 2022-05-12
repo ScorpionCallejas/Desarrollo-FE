c3954baee0f5eca0d6e39fd54ce97cc54325c5ad
ScorpionCallejas <scorpionmorales17@gmail.com>
2022-05-11 20:11:15 -0500

    Se agrego un widget

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Widget de fecha y hora
+function refresh(){
+    // Script para mostrar la fecha
+    const date = new Date();
+    // Array de los meses del año
+    let month = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    // Array de los dias de la semana
+    let day = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    // Formullación de el formato en el que se mostrara la fecha actual
+    const dateComplete = (day[date.getDay()] + ", " + date.getDate() + " de " + month[date.getMonth()] + " de " + date.getUTCFullYear());
+    // Mostrar la fecha actual
+    document.getElementById("date").innerHTML = dateComplete;
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if(hours < 10){hours = "0" + hours;}
+    if(minutes < 10){minutes = "0" + minutes;}
+    if(seconds < 10){seconds = "0" + seconds;}
+    let hoursComplete = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("hours").innerHTML = hoursComplete;
+
+    let id = document.getElementById("widget-time");
+    if(hours < 18){
+        id.classList.add("day");
+    }
+    else if(hours >= 18){
+        id.classList.add("night");
+    }
+}
+setInterval(refresh, 1000);
